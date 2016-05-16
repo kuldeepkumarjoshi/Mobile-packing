5974cb1d32a08b14ea447e9d41e9ae1490217cef
kuldeepkumarjoshi <kuldeep.joshi@metacube.com>
2016-05-17 00:18:34 +0530

    home page ui

--- www/js/filters.js
+++ www/js/filters.js
@@ -3,12 +3,19 @@ angular.module('woocommerce-api.filters', [])
 .filter('partition', function ($cacheFactory) {
     var arrayCache = $cacheFactory('partition');
     var filter = function (arr, size) {
+      //size =1;
         if (!arr) {
             return;
         }
         var newArr = [];
         for (var i = 0; i < arr.length; i += size) {
-            newArr.push(arr.slice(i, i + size));
+          if((i+1)%3 == 0){
+              newArr.push(arr.slice(i, i + 1));
+              i = i -size+1;
+          }else{
+              newArr.push(arr.slice(i, i + size));
+          }
+
         }
         var cachedParts;
         var arrString = JSON.stringify(arr);
@@ -47,7 +54,7 @@ angular.module('woocommerce-api.filters', [])
 })
 
 .filter('partitionC', function ($cacheFactory) {
-    var arrayCache = $cacheFactory('partition')
+    var arrayCache = $cacheFactory('partitionC')
     return function (arr, size) {
         var parts = [],
             cachedParts,
