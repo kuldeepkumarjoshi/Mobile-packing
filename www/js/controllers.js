8c7d37243b0258bf6efade3c34a2debb35dc120c
kuldeepkumarjoshi <kuldeep.joshi@metacube.com>
2016-05-17 19:05:49 +0530

    subcategory page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,13 +19,14 @@ angular.module('woocommerce-api.controllers', [])
 .controller('SubcategoriesCtrl', function($scope, $rootScope, Data, UserData, MetaData,CategoriesData) {
   $scope.isEmptyData = true;
     $scope.categories = [];
+    
 $rootScope.$broadcast('loading:show');
   if($rootScope.cat != null){
     $scope.isEmptyData = false;
-      $scope.categories.push($rootScope.cat);
+      $scope.categories=$rootScope.cat;
   }
 $rootScope.$broadcast('loading:hide');
-
+console.log(  $scope.categories);
 $scope.getPercentageValue = function(value, total) {
     return value * 100 / total;
 }
